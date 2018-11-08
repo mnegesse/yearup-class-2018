1f2b52fd1508d6929e25f6033802a9b9b2d96b32
Mikias Negesse <mnegesse@nyc.yearup.org>
2018-11-08 16:38:25 -0500

    workshop commit

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,11 @@
 //This comes from data.js
 var gifs = window.data.data;
 
+function ranDom(){
+  let select = Math.floor(Math.random()*gifs.length);
+  console.log(select);
+  let disply = document.getElementById('fox');
+  disply.src = gifs[select].images.original.url;
+}
+let button = document.querySelector('button');
+button.addEventListener('click', ranDom);
